814e6b4da838eab82a9577bd73ebd7b616ab5a96
afokay <ahmadnitro@proton.me>
2023-08-13 11:17:48 +0700

    Update

--- _config.ts
+++ _config.ts
@@ -3,7 +3,7 @@ import metas from "lume/plugins/metas.ts";
 import postcss from "lume/plugins/postcss.ts";
 
 const site = lume({
-    location: new URL("https://dry-donkey-75.deno.dev"),
+    location: new URL("https://lume-gooks.deno.dev"),
     components: {
         variable: "components",
     },
